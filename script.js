952cb474d0335a8d603fcba6dfc9995002852683
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-08-17 13:42:33 +0800

    worked on site

--- script.js
+++ script.js
@@ -13,8 +13,18 @@ const data = {
 let savedSettings = localStorage.getItem('WaceDatabaseSearchSettings');
 if (savedSettings) data.filters = JSON.parse(savedSettings);
 
+function toggleContent(id) {
+    const extraContent = document.getElementById(`extraContent${id}`);
+    const button = document.getElementById(`button${id}`);
+    if (extraContent.style.display === "none" || !extraContent.style.display) {
+        extraContent.style.display = "block";
+        button.classList.add("active");
+    } else {
+        extraContent.style.display = "none";
+        button.classList.remove("active");
+    }
+}
 
-// Get Data
 async function loadJson(path) {
     let fetched = null;
     await fetch(`./${path}.json`).then(response => {
@@ -28,6 +38,16 @@ async function loadJson(path) {
         console.error('Error fetching the JSON file:', error);
     });
     return fetched;
+}
+
+async function setTags() {
+    const allTags = await loadJson('tags');
+    const tagsList = allTags[data.filters.subject];
+    let tagsHtml = ``;
+    for (let tag of tagsList) {
+        tagsHtml += `<label class="tag"><input type="checkbox" id="${tag}" class="tagSelect"><span class="tagLabel">${tag}</span></label>`
+    }
+    document.getElementById('tagsContainer').innerHTML = tagsHtml;
 }
 
 async function search() {
@@ -57,14 +77,12 @@ async function search() {
     settingsChanged = false;
 }
 
-async function setTags() {
-    const allTags = await loadJson('tags');
-    const tagsList = allTags[data.filters.subject];
-    let tagsHtml = ``;
-    for (let tag of tagsList) {
-        tagsHtml += `<label class="tag"><input type="checkbox" id="${tag}" class="tagSelect"><span class="tagLabel">${tag}</span></label>`
-    }
-    document.getElementById('tagsContainer').innerHTML = tagsHtml;
+async function toggleKey(id) {
+    const imageContainer = document.getElementById(`question${id}`);
+    let image = imageContainer.innerHTML;
+    image = image.replace('questionBank', '[key]').replace('markingKeys', '[question]');
+    image = image.replace('[key]', 'markingKeys').replace('[question]', 'questionBank');
+    imageContainer.innerHTML = image;
 }
 
 setTags();
