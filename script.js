419ee387cc7abd1cf8228c7eb054c1429a8085bd
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-08-18 14:02:28 +0800

    debugging

--- script.js
+++ script.js
@@ -81,7 +81,9 @@ async function search() {
     for (let i in data.questions) {
         questionsHtml += `<div id="result${i}" class="box whiteBackground"><div class="resultTopRow"><h3 class="alignLeft">${data.questions[i].name}</h3><span class="alignRight"><button id="button${i}" class="toggleButton" onclick="toggleContent(${i})">▼</button></span></div><div class="extraContent" id="extraContent${i}"><div id="question${i}" class="questionArea"><img src="questionBank/spec/${data.questions[i].id}.webp" class="questionImage"></div><button class="standardButton" onclick="toggleKey(${i})">Show Marking Key</button><a href="pdfDownloads/spec/${data.questions[i].id}.pdf" download="${data.questions[i].id}.pdf"><button class="standardButton">Download PDF</button></a></div></div>`;
     }
+    console.log(questionsHtml);
     if (questionsHtml == ``) questionsHtml = `<h3>No Results Found</h3>`;
+    console.log(questionsHtml);
     document.getElementById('searchResults').innerHTML = questionsHtml;
 }
 
