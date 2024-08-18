e8c099f9b552b2e1e70e12da8ae6483519b9629b
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-08-18 22:23:02 +0800

    3 years of physics exams (some bugs present)

--- autoQuestionData.py
+++ autoQuestionData.py
@@ -1,15 +1,15 @@
 template = '''{
-    "id": "WACE2017Q[i]",
-    "name": "WACE 2017 Calc Free Question [i]",
-    "year": 2017,
+    "id": "WACE2021Q[i]",
+    "name": "WACE 2021 Question [i]",
+    "year": 2021,
     "source": "WACE",
-    "calculator": "free",
+    "calculator": "assumed",
     "tags": []
 },'''
 
 start = 1
-end = 8
-
+end = 21
+# 20 19 21
 i = start
 while i <= end:
     print(template.replace('[i]', str(i)))
