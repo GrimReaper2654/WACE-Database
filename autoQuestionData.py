8fe68f9b789d712e2e211aed0c4a50f31be3eb45
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-08-19 11:41:21 +0800

    add econs 2023 untagged

--- autoQuestionData.py
+++ autoQuestionData.py
@@ -1,15 +1,15 @@
 template = '''{
-    "id": "WACE2021Q[i]",
-    "name": "WACE 2021 Question [i]",
-    "year": 2021,
+    "id": "WACE2023Q[i]",
+    "name": "WACE 2023 Question [i]",
+    "year": 2023,
     "source": "WACE",
     "calculator": "assumed",
     "tags": []
 },'''
 
 start = 1
-end = 21
-# 20 19 21
+end = 31
+
 i = start
 while i <= end:
     print(template.replace('[i]', str(i)))
