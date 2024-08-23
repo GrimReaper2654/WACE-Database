c764caf649505ea407f0222b1baf5ad2e17c5bc7
GrimReaper2654 <80506189+GrimReaper2654@users.noreply.github.com>
2024-08-24 02:23:45 +0800

    semiautomated screenshots

---
+++ imageProcessingArea/screenshotter.py
@@ -0,0 +1,25 @@
+import pyautogui
+import keyboard
+
+'''
+This screenshot progam works on Mac only. You will need to change the keybind if you are using a different OS.
+This program needs to control your computer to screenshot so it may need more perms. Sudo should work.
+run in terminal at the image processing folder: sudo python3 screenshotter.py 
+'''
+
+def press_screenshot_keybind():
+    # Simulate pressing Command + Shift + 5
+    pyautogui.hotkey('command', 'shift', '5')
+
+def on_c_key():
+    press_screenshot_keybind()
+
+# Bind the 'c' key to trigger the screenshot keybind
+keyboard.add_hotkey('c', on_c_key)
+
+print("Press 'c' to trigger the screenshot keybind (Command + Shift + 5).")
+print("Press 'esc' to exit the program.")
+
+# Keep the script running
+keyboard.wait('esc')
+
